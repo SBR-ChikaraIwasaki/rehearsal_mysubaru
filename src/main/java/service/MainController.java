cc172ec6a201b64494ebfad03eaef6e62b38ebe6
my_subaru_config01 <my_subaru_config01@my-subaru-config01noMacBook-Pro.local>
2019-04-05 17:32:35 +0900

    feature02

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-    // add feature02
     @RequestMapping("/")
     String hello() {
         return "hello world";
     }
     // feature01 add
+    // add feature02
 }
